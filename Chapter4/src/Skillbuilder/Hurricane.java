68c0ac57b6f3fa2d6c94671fdf7c79dc9b0d266f
1100107595 <1100107595@L815WD220020.edu.cbe.ab.ca>
2024-10-03 10:18:49 -0600

    10:18AM

---
+++ Chapter4/src/Skillbuilder/Hurricane.java
@@ -0,0 +1,13 @@
+package Skillbuilder;
+import java.util.Scanner;
+public class Hurricane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+
+	}
+
+}
+
