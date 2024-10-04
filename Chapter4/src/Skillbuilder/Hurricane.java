469ec71138ec79bfbdfc650f1996199c79378035
1100107595 <1100107595@L815WD220020.edu.cbe.ab.ca>
2024-10-04 10:50:12 -0600

    si

--- Chapter4/src/Skillbuilder/Hurricane.java
+++ Chapter4/src/Skillbuilder/Hurricane.java
@@ -4,10 +4,21 @@ public class Hurricane {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//prompt the user for the category
 		Scanner input = new Scanner(System.in);
-
+		System.out.print("Which category is your hurricane?: ");
+        int catg = input.nextInt();
+        input.close();
+        System.out.println();
+        switch (catg) {
+        case 1: System.out.print("The speed is: 74-95mph or 64-82kt or 119-153km/h"); break;
+        case 2: System.out.print("The speed is: 96-110mph or 83-95kt or 154-177km/h"); break;
+        case 3: System.out.print("The speed is: 111-130mph or 96-113kt or 178-209km/h"); break;
+        case 4: System.out.print("The speed is: 131-150mph or 114-135kt or 210-249km/h"); break;
+        case 5: System.out.print("The speed is: greater than 150mph or 135kt or 249km/h"); break;
+        }
 	}
 
 }
 
+//listo mlp
--- Chapter4/src/Skillbuilder/RandNum.java
+++ Chapter4/src/Skillbuilder/RandNum.java
@@ -1,10 +1,29 @@
 package Skillbuilder;
 
+import java.lang.Math;
+import java.util.Scanner;
+
 public class RandNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Prompt user for minimum and maximum values
+        System.out.print("Please enter the minimum value: ");
+        int minVal = input.nextInt();
 
-	}
+        System.out.print("Please enter the maximum value: ");
+        int maxVal = input.nextInt();
 
+        input.close();
+        // Check if the minimum is less than the maximum
+        if (minVal >= maxVal) {
+            System.out.println("Error: Minimum value must be less than maximum value.");
+        } else {
+            // Display the random integer
+            System.out.println("Random integer between " + minVal + " and " + maxVal + ": " + ((minVal-maxVal+1)* MathRandom()+minVal));
+        }
+
+    }
+    
 }
+
