8f4a4040b4671fd6eb21f12cf656616ee4e6d44d
1100107595 <1100107595@L815WD220020.edu.cbe.ab.ca>
2024-10-16 10:36:27 -0600

    10:36

---
+++ Chapter5/src/Skillbuilder/Evens.java
@@ -0,0 +1,17 @@
+package Skillbuilder;
+
+public class Evens {
+
+    public static void main(String[] args) {
+    	
+        System.out.println("Even numbers from 1 to 20:");
+        
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+        }
+            
+        } 
+    }
+}
+    
---
+++ Chapter5/src/Skillbuilder/NumbersSum.java
@@ -0,0 +1,11 @@
+package Skillbuilder;
+
+public class NumbersSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//dom
+		
+	}
+
+}
