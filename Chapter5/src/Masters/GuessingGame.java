2a140c0b6f655cc68eec1f422e29586967e5a4d6
1100107595 <1100107595@L815WD220020.edu.cbe.ab.ca>
2024-10-21 11:29:17 -0600

    11:28

---
+++ Chapter5/src/Masters/GuessingGame.java
@@ -0,0 +1,62 @@
+/*
+
+Program: GuessingGame.java    Date: October 21, 2024
+
+Purpose: generate a random number that the user should guess.
+
+Author: Cesar Pena, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
+package Masters;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNum = random.nextInt(20) + 1; // generate a random number
+        int userNum = 0; // initialize userNum
+        int attempts = 0;
+
+        System.out.println("Guess the number between 1 and 20!");
+
+        while (userNum != secretNum) {
+            System.out.print("Enter a number between 1 and 20: ");
+            // Validate input
+            if (input.hasNextInt()) {
+                userNum = input.nextInt();
+
+                if (userNum < 1 || userNum > 20) {
+                    System.out.println("Please enter a number within the range.");
+                    continue;
+                }
+
+                attempts++;
+                
+                // Compare both numbers
+                if (userNum == secretNum) {
+                    System.out.println("You won! It took you " + attempts + " attempts.");
+                } else if (userNum < secretNum) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            } else {
+                System.out.println("That's not a valid number. Please enter a number between 1 and 20.");
+                input.next(); // Clear the invalid input
+            }
+        }
+
+        System.out.printf("Computer's number: %d\n", secretNum);
+        input.close();
+    }
+}
+
