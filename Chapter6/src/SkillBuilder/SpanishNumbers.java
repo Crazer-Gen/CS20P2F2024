2351418f99c6070ee61ec58c63322b2b4c90fb1e
1100107595 <1100107595@L815WD220020.edu.cbe.ab.ca>
2024-10-22 10:20:47 -0600

    10:20

---
+++ Chapter6/src/SkillBuilder/SpanishNumbers.java
@@ -0,0 +1,10 @@
+package SkillBuilder;
+
+public class SpanishNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter6/src/SkillBuilder/TimeConverter.java
@@ -0,0 +1,10 @@
+package SkillBuilder;
+
+public class TimeConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
