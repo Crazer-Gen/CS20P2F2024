d3245c306d18935f7dd31a30e176d5f2c86076c4
1100107595 <1100107595@L815WD220020.edu.cbe.ab.ca>
2024-10-22 09:36:25 -0600

    9:36

--- Chapter5/src/Masters/GroupAssignement.java
+++ Chapter5/src/Masters/GroupAssignement.java
@@ -1,9 +1,37 @@
+/*
+
+Program: GroupAssignement.java    Date: October 21, 2024
+
+Purpose: Assign a group to the user depending on her/his name
+
+Author: Cesar Pena,
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
 package Masters;
 
+import java.util.Scanner;
+import java.lang.String;
 public class GroupAssignement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		//prompt the user for his/her name
+		System.out.print("Enter your first name: ");
+		String FName = input.nextLine();
+		System.out.print("Enter your last name: ");
+		String LName = input.nextLine();
+		input.close();
+		//get the first letter of the user's last name
+		int Go = LName.length();
+		int DetLet = Go.substring(LName-1);
+		//based on "flounln" determine the group for the user
+		switch (Go) {
+		case A
+		}
 
 	}
 
